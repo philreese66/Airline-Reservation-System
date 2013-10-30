9fc1bb97e01535c5267fafaa9edadb51ddc16d70
Philip <Philip@PhilsLaptop>
2013-10-29 20:30:27 -0500

    I made some more code changes

--- SeatReservation/src/images/NewLayout.java
+++ SeatReservation/src/images/NewLayout.java
@@ -8,7 +8,8 @@ package images;
  *
  * @author Philip
  */
-
+import java.awt.event.*;
+import java.awt.*;
 import java.awt.Color;
 import javax.swing.JTabbedPane;
 import javax.swing.JLabel;
@@ -21,16 +22,18 @@ import javax.swing.JFrame;
 import javax.swing.BoxLayout;
 import java.awt.Dimension;
 import javax.swing.Box;
+import javax.swing.JOptionPane;
 
 
 
-public class NewLayout extends java.awt.Frame {
+public class NewLayout extends java.awt.Frame implements ActionListener{
 
-    int size = 3;
+    int size = 3, section, first = 0, econ = 5, replyFirst, replyEconomy, dialogButton;
     JLabel label1, labelInstructions, labelInstructions2, labelInstructions3, labelInstructions4;
     JPanel panel1, panel2, panel3, panel4, panel5, panel6;
     JButton submitButton;
     JTextField userClass, seatNumber, statusClass;
+    boolean seats[];
     /**
      * Creates new form NewLayout
      */
@@ -39,8 +42,7 @@ public class NewLayout extends java.awt.Frame {
         super ( "NHCC Airline Seat Reservation" );
         
         initComponents();
-        
-        
+                
         JTabbedPane tabbedPane = new JTabbedPane();
         tabbedPane.setSize( 625, 405);
         
@@ -53,14 +55,14 @@ public class NewLayout extends java.awt.Frame {
         panel5 = new JPanel ();
         panel6 = new JPanel ();
         submitButton = new JButton("Submit");
+        submitButton.addActionListener(this);
+
         userClass = new JTextField("", 15);
         userClass.setMaximumSize( userClass.getPreferredSize() );
         seatNumber = new JTextField("", 15);
         seatNumber.setMaximumSize( userClass.getPreferredSize() );
         statusClass = new JTextField("",15);
         statusClass.setMaximumSize( userClass.getPreferredSize() );
-        
-        
         
         Box vertical1 = Box.createVerticalBox();
         vertical1.add ( Box.createRigidArea ( new Dimension(12,4)));
@@ -84,17 +86,10 @@ public class NewLayout extends java.awt.Frame {
         panel5.add(labelInstructions4);
         panel5.add(Box.createRigidArea(new Dimension(0,15)));
         panel5.add(statusClass);
-        
-        
-        
-        
-        
+              
         panel1 = new JPanel();
-        
         panel1.setLayout (new BorderLayout ());
-        
         panel1.add( label1, BorderLayout.NORTH);
-        
         panel1.add (panel4, BorderLayout.WEST);
         panel1.add (panel5, BorderLayout.EAST);
         tabbedPane.addTab ("Reserve Seats", null, panel1, "First Panel");
@@ -120,6 +115,77 @@ public class NewLayout extends java.awt.Frame {
         
         
         add (tabbedPane);
+    }
+    //private void classSubmitButtonActionPerformed(java.awt.event.ActionEvent evt)
+    //public void actionPerformed(ActionEvent evt)
+    //private void submitButtonActionPerformed(java.awt.event.ActionEvent evt)
+    public void actionPerformed(ActionEvent evt)
+    {
+        seats = new boolean [10];
+        for ( int index = 0; index < seats.length; index++ )
+            seats[ index ] = false;
+      
+        
+        if (evt.getSource() == submitButton)
+        {
+            section = Integer.parseInt(userClass.getText());
+        }
+        if ( section == 1 )
+        {
+          if (first < 5) 
+            {
+                seats[first] = true;
+                statusClass.setText("First Class");
+                seatNumber.setText("Seat #" + ++first); 
+            }
+            else
+            {   
+                dialogButton = JOptionPane.YES_NO_OPTION;
+                replyFirst = JOptionPane.showConfirmDialog(this, "First class is full. Would you like a seat in economy class?", "No seats available",dialogButton);
+                    
+                    if (replyFirst == JOptionPane.YES_OPTION)
+                        {
+                            seats[ econ ] = true;
+                            statusClass.setText("Economy Class");
+                            seatNumber.setText("Seat #" + ++econ);
+                        }
+                    if (replyFirst == JOptionPane.NO_OPTION)
+                        {
+                JOptionPane.showMessageDialog(null,"Thank you for your time, Please check back again");
+                        }
+                
+                //classOutput.setText("First Class is full");     
+            }
+        }
+        if ( section == 2 )
+        {
+            if ( econ < 10 ) 
+            {
+                seats[ econ ] = true;
+                statusClass.setText("Economy Class");
+                seatNumber.setText("Seat #" + ++econ);
+            }
+            else
+            {    dialogButton = JOptionPane.YES_NO_OPTION;
+                replyEconomy = JOptionPane.showConfirmDialog(this, "Economy class is full. Would you like a seat in first class?", "No seats available",dialogButton);
+                    
+                    if (replyEconomy == JOptionPane.YES_OPTION)
+                        {
+                            seats[ first ] = true;
+                            statusClass.setText("First Class");
+                            seatNumber.setText("Seat #" + ++first);
+                        }
+                    if (replyEconomy == JOptionPane.NO_OPTION)
+                        {
+                JOptionPane.showMessageDialog(null,"Thank you for your time, Please check back again");
+                        }
+            }
+                //classOutput.setText("Economy is full");
+        
+            //else 
+                //classOutput.setText("Invalid Class");
+                
+        }  
     }
 
     /**
